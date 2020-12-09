2cd37a4bf487ee0a3950a7bf26e33a9106b81406
marit <75077118+Mar-Git@users.noreply.github.com>
2020-12-09 18:51:16 +0100

    He añadido hola mundo

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -2,8 +2,8 @@
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		System.out.println("Mar Acal Criado");//coment
-
+		System.out.println("Mar Acal Criado");
+		System.out.println("Hola mundo");
 	}
 
 }
